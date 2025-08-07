d99984d05c72ea57d03589970ebb16fe09538c0f
Maksym Pashchenko <maksympash@knu.ua>
2025-08-07 20:13:36 +0300

    More methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
 let bob = 'Hello, world!';
-console.log(bob.toLowerCase())
-console.log(bob.toUpperCase())
-console.log(bob)
+console.log(bob[bob.length - 1])
+
+
+let bobser = '    Hello, world!     ';
+console.log(bobser.trim());
+
+let bobik = 'Hello, world!';
+console.log(bobik.slice(0, 5))
