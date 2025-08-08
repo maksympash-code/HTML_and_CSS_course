780178c1ec00e4db8506dbbdf63d251af772af35
Maksym Pashchenko <maksympash@knu.ua>
2025-08-08 06:54:30 +0300

    Round methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log(Math.random())
+console.log(Math.floor(3.9));
+console.log(Math.ceil(3.1));
+console.log(Math.round(3.1));
+console.log(Math.round(3.9));
+console.log(Math.trunc(5.99293943949));
+console.log(3.92132.toFixed(2));
+
+let a = '6';
+
+console.log(a + 6);
+console.log(+a + 6);
