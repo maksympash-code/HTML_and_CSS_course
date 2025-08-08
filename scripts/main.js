063680ca209546c513cfa999824c5c0dd9ee30f0
Maksym Pashchenko <maksympash@knu.ua>
2025-08-08 20:28:06 +0300

    Object

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
-let array1 = [1, 2, 3];
-let array2 = [3, 4];
-let array3 = array1.concat(array2);
-let idx = array2.indexOf(5);
-let array4 = array2.join(' / ');
-let string1 = array4.split(' / ');
+let obj = {
+    'color': 'red',
+    'number': 12,
+    'snow': true,
+    'numbers': [123, 345, 678],
+    'age': 19,
+};
 
-console.log(array3); // [1,2,3] + [4, 5] = [1,2,3,4,5]
-console.log(idx); // element index in massive
-console.log(array4); // between massive to string
-console.log(string1);
+console.log(obj);
+console.log(obj.numbers);
+console.log(obj.numbers[1]);
+console.log(Object.keys(obj))
