0f9f3c4f1b003b1ba231b2939f82614edc6881e3
Maksym Pashchenko <maksympash@knu.ua>
2025-08-08 07:02:04 +0300

    Create functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
-console.log(Math.floor(3.9));
-console.log(Math.ceil(3.1));
-console.log(Math.round(3.1));
-console.log(Math.round(3.9));
-console.log(Math.trunc(5.99293943949));
-console.log(3.92132.toFixed(2));
+function print() {
+    console.log('Hello, World!');
+};
 
-let a = '6';
-
-console.log(a + 6);
-console.log(+a + 6);
+print();
+print();
+print();
