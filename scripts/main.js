a8e7673991601038c1ad76e2929f951172c0163e
Maksym Pashchenko <maksympash@knu.ua>
2025-08-08 20:22:22 +0300

    Some massive methods

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
-let array = [1, 2, 3, 4, 5];
+let array1 = [1, 2, 3];
+let array2 = [3, 4];
+let array3 = array1.concat(array2);
+let idx = array2.indexOf(5);
+let array4 = array2.join(' / ');
+let string1 = array4.split(' / ');
 
-console.log(array.pop());
-console.log(array.pop());
-console.log(array.pop());
-
-console.log(array);
-
-array.push(6);
-console.log(array);
+console.log(array3); // [1,2,3] + [4, 5] = [1,2,3,4,5]
+console.log(idx); // element index in massive
+console.log(array4); // between massive to string
+console.log(string1);
