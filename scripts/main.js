930a070d33748ea5365fc2e32c9268a50b96132b
Maksym Pashchenko <maksympash@knu.ua>
2025-08-09 06:24:23 +0300

    Solution at cycle while()

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 let elems = document.querySelectorAll('div');
 
-for(let i of elems){
-    i.innerHTML += ' <b style = "color: orange">Something my text</b>'
-    i.style.background = 'green';
-    i.style.color = '#fff';
+
+let i = 0
+while(i < elems.length){
+    elems[i].innerHTML += ' <b style = "color: orange">Something my text</b>'
+    elems[i].style.background = 'green';
+    elems[i].style.color = '#fff';
+    i++;
 }
